71c57c5018d4ca0a909c0b342dc329dd9621c6e1
Jayabalan-D <jb.jayabalan2006@gmail.com>
2025-09-07 14:53:04 +0530

    Update credentials.h

--- hardware-code/credentials.h
+++ hardware-code/credentials.h
@@ -1,1 +1,6 @@
+// WiFi & Adafruit IO credentials
+#define WIFI_SSID       "Balas_MOTO"
+#define WIFI_PASS       "Jaya@555"
+#define AIO_USERNAME    "Jayabalan_D"
+#define AIO_KEY         "aio_hfuJ60EY4fepMVsMn3NOCeaml2UR"
 
