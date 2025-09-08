5a22ff4c47656c70f82023f7cfcc0823ca841197
Jayabalan-D <jb.jayabalan2006@gmail.com>
2025-09-08 20:32:54 +0530

    Update script.js

--- web-app/script.js
+++ web-app/script.js
@@ -1,1 +1,31 @@
+const USERNAME = "Adafruit username";  // your Adafruit username
+const AIO_KEY = "Adafruit IO Key"; // your Adafruit IO Key
+
+const client = mqtt.connect("wss://io.adafruit.com:443/mqtt", {
+  username: USERNAME,
+  password: AIO_KEY
+});
+
+client.on("connect", () => {
+  console.log("✅ Connected to Adafruit IO");
+  client.subscribe(`${USERNAME}/feeds/gas_level`);
+  client.subscribe(`${USERNAME}/feeds/humidity`);
+  client.subscribe(`${USERNAME}/feeds/temperature`);
+  client.subscribe(`${USERNAME}/feeds/Usage_count`);
+});
+
+client.on("message", (topic, message) => {
+  const value = message.toString();
+  console.log(`📡 ${topic}: ${value}`);
+
+  if (topic.endsWith("gas_level")) {
+    document.getElementById("aq").innerText = value;
+  } else if (topic.endsWith("humidity")) {
+    document.getElementById("hum").innerText = value + " %";
+  } else if (topic.endsWith("temperature")) {
+    document.getElementById("temp").innerText = value + " °C";
+  } else if (topic.endsWith("Usage_count")) {
+    document.getElementById("usage").innerText = value;
+  }
+});
 
