fc17fa7f16e0d9707939ef8cf283719274e04b2d
Jayabalan-D <jb.jayabalan2006@gmail.com>
2025-09-08 20:40:14 +0530

    Delete hardware-code/credentials.h

--- hardware-code/credentials.h
+++
@@ -1,9 +0,0 @@
-#ifndef CREDENTIALS_H
-#define CREDENTIALS_H
-
-#define WIFI_SSID    "Balas_MOTO"
-#define WIFI_PASS    "Jaya@555"
-#define IO_USERNAME  "Jayabalan_D"
-#define IO_KEY       "aio_hfuJ60EY4fepMVsMn3NOCeaml2UR"
-
-#endif
